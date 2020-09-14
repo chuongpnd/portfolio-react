c443a290505927b38f790831ff9a27e83e14bca7
Chuongpnd <doanchuong.phamngoc@officience.com>
2020-09-14 23:48:58 +0700

    Updates

---
+++ precache-manifest.10d6c3b97a92c35971f08a7186aae13e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6b0ec69faadc74c11b1254a99d3e2c",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "cee53aeb77c6037bfcd5",
+    "url": "/portfolio-react/static/css/main.f773e216.chunk.css"
+  },
+  {
+    "revision": "48db949c0b99e6b05de1",
+    "url": "/portfolio-react/static/js/2.d8b0744f.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio-react/static/js/2.d8b0744f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee53aeb77c6037bfcd5",
+    "url": "/portfolio-react/static/js/main.a2ad9208.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  },
+  {
+    "revision": "90ae8f05fdd930e4262f85afaada5741",
+    "url": "/portfolio-react/static/media/avatar-cp.90ae8f05.jpeg"
+  },
+  {
+    "revision": "44296fded78b39ee6f5f811295029a39",
+    "url": "/portfolio-react/static/media/developer.44296fde.png"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/portfolio-react/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/portfolio-react/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/portfolio-react/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/portfolio-react/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/portfolio-react/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/portfolio-react/static/media/react.f370c1e0.png"
+  }
+]);
